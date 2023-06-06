d7f73d5b6b7c54cb799b1fff236c1478451b57eb
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-06-06 08:56:10 -0300

    manipulação de API

---
+++ src/TrackSection/TrackList/index.jsx
@@ -0,0 +1,13 @@
+import { TrackCard } from "./TrackCard"
+import { StyledDiv } from "./styles"
+
+export const TrackList = ({ tracks }) => {
+    return (
+        <StyledDiv>
+            {tracks.map((track) => (
+                <TrackCard key={track.id} track={track} />
+            ))}
+
+        </StyledDiv>
+    )
+}
---
+++ src/TrackSection/index.jsx
@@ -0,0 +1,9 @@
+import { TrackList } from "./TrackList"
+
+export const TrackSection = ({ tracks }) => {
+    return (
+        <section>
+            <TrackList tracks={tracks} />
+        </section>
+    )
+}
